bc0cc28aa83fd85bc5aac6362c70a9fbfbd0df47
yalh76 <yalh@yahoo.com>
2019-03-14 01:10:25 +0100

    Apply last example_ynh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 #=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+pkg_dependencies="git build-essential openssl ssh sudo postgresql postgresql-contrib elixir erlang-dev erlang-parsetools erlang-xmerl erlang-tools"
+
+#=================================================
 # PERSONAL HELPERS
 #=================================================
 
