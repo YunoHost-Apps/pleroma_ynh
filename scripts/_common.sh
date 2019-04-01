94dab0ed3ff3d811d482dd8446e06b347811a5d6
yalh76 <yalh@yahoo.com>
2019-04-02 01:36:58 +0200

    add ynh_add_secure_repos__3

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,8 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="git build-essential postgresql postgresql-contrib elixir erlang-dev erlang-tools erlang-parsetools erlang-eldap erlang-xmerl openssl ssh sudo"
+pkg_dependencies="git build-essential postgresql postgresql-contrib openssl ssh sudo"
+extra_pkg_dependencies="elixir erlang-dev erlang-tools erlang-parsetools erlang-eldap erlang-xmerl"
 
 #=================================================
 # PERSONAL HELPERS
