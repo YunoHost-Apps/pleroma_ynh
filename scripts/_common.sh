32389bd62537dc83c78b59fdb521f8fae56195dc
yalh76 <yalh@yahoo.com>
2019-04-18 12:21:26 +0200

    fix upgrade

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 # dependencies used by the app
 pkg_dependencies="git build-essential postgresql postgresql-contrib openssl ssh sudo"
-extra_pkg_dependencies="elixir esl-erlang"
+extra_pkg_dependencies="elixir erlang-dev erlang-tools erlang-parsetools erlang-eldap erlang-xmerl esl-erlang"
 
 #=================================================
 # PERSONAL HELPERS
