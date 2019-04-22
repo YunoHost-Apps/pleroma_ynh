17d520c03b424f9bd01f901d7ac273493dfd9dc2
yalh76 <yalh@yahoo.com>
2019-04-22 18:26:08 +0200

    add  erlang-eldap dependency

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,8 +6,8 @@
 
 # dependencies used by the app
 pkg_dependencies="git build-essential postgresql postgresql-contrib openssl ssh sudo"
-extra_pkg_dependencies="elixir esl-erlang"
-pkg_dependencies_to_remove="erlang-dev erlang-tools erlang-parsetools erlang-eldap erlang-xmerl"
+extra_pkg_dependencies="elixir esl-erlang erlang-eldap"
+pkg_dependencies_to_remove="erlang-dev erlang-tools erlang-parsetools erlang-xmerl"
 
 #=================================================
 # PERSONAL HELPERS
