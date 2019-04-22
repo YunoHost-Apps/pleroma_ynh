4b1aa3897990664792baecc70ad255fcb8e3d30e
yalh76 <yalh@yahoo.com>
2019-04-22 19:28:16 +0200

    replacing depenencies removal by apt autoremove

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,6 @@
 # dependencies used by the app
 pkg_dependencies="git build-essential postgresql postgresql-contrib openssl ssh sudo"
 extra_pkg_dependencies="elixir esl-erlang erlang-eldap"
-pkg_dependencies_to_remove="erlang-dev erlang-tools erlang-parsetools erlang-xmerl"
 
 #=================================================
 # PERSONAL HELPERS
