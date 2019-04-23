93b9a4fee90491166601596dcc7e69c9610cdc9d
yalh76 <yalh@yahoo.com>
2019-04-23 12:17:30 +0200

    reorder dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 # dependencies used by the app
 pkg_dependencies="git build-essential postgresql postgresql-contrib openssl ssh sudo"
-extra_pkg_dependencies="elixir erlang-dev erlang-tools erlang-parsetools erlang-xmerl erlang-eldap"
+extra_pkg_dependencies="elixir erlang-dev erlang-tools erlang-parsetools erlang-eldap erlang-xmerl"
 
 #=================================================
 # PERSONAL HELPERS
