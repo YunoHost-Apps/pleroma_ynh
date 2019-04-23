e676157ec528dba9cd6120dcc53ebae9714bb8bf
yalh76 <yalh@yahoo.com>
2019-04-23 12:16:16 +0200

    remove esl-erlang dependenciy switch

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,8 +6,7 @@
 
 # dependencies used by the app
 pkg_dependencies="git build-essential postgresql postgresql-contrib openssl ssh sudo"
-extra_pkg_dependencies="elixir esl-erlang erlang-eldap"
-pkg_dependencies_to_remove="erlang-dev erlang-tools erlang-parsetools erlang-xmerl"
+extra_pkg_dependencies="elixir erlang-dev erlang-tools erlang-parsetools erlang-xmerl erlang-eldap"
 
 #=================================================
 # PERSONAL HELPERS
