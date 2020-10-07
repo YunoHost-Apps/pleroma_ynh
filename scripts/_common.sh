dbce35860b19df6878601d7b27100729910a48de
yalh76 <yalh@yahoo.com>
2020-10-08 00:36:20 +0200

    Installing optional packages

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="curl unzip libncurses5 postgresql postgresql-contrib"
+pkg_dependencies="curl unzip libncurses5 postgresql postgresql-contrib imagemagick ffmpeg libimage-exiftool-perl"
 
 #=================================================
 # PERSONAL HELPERS
