72b8ab54a63ecf667ef8b87deebc440dc0fb9c89
yalh76 <yalh@yahoo.com>
2020-12-22 21:26:57 +0100

    Adding  exiftool

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="curl unzip libncurses5 postgresql postgresql-contrib imagemagick ffmpeg libimage-exiftool-perl"
+pkg_dependencies="curl unzip libncurses5 postgresql postgresql-contrib imagemagick ffmpeg exiftool libimage-exiftool-perl"
 
 #=================================================
 # PERSONAL HELPERS
