86fb8190fa4c1177edfee2dc613f083950c3efed
ericgaspar <junk.eg@free.fr>
2021-08-23 08:04:19 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -18,3 +18,27 @@ pkg_dependencies="curl unzip libncurses5 postgresql postgresql-contrib"
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
+
+# Check the architecture
+#
+# example: architecture=$(ynh_detect_arch)
+#
+# usage: ynh_detect_arch
+#
+# Requires YunoHost version 2.2.4 or higher.
+
+ynh_detect_arch(){
+	local architecture
+	if [ -n "$(uname -m | grep arm64)" ] || [ -n "$(uname -m | grep aarch64)" ]; then
+		architecture="arm64"
+	elif [ -n "$(uname -m | grep 64)" ]; then
+		architecture="x86-64"
+	elif [ -n "$(uname -m | grep 86)" ]; then
+		architecture="i386"
+	elif [ -n "$(uname -m | grep arm)" ]; then
+		architecture="arm"
+	else
+		architecture="unknown"
+	fi
+	echo $architecture
+}
