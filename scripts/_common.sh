9df5a996d8e4abd3c7bd841985bd5e6c76ffc0c6
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-05-31 21:32:34 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app (must be on a single line)
-#REMOVEME? pkg_dependencies="curl unzip libncurses5 postgresql postgresql-contrib imagemagick ffmpeg exiftool libimage-exiftool-perl"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
@@ -18,3 +15,28 @@
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
+
+read_json () {
+    sudo python3 -c "import sys, json;print(json.load(open('$1'))['$2'])"
+}
+
+read_manifest () {
+    if [ -f '../manifest.json' ] ; then
+        read_json '../manifest.json' "$1"
+    else
+        read_json '../settings/manifest.json' "$1"
+    fi
+}
+abort_if_up_to_date () {
+    version=$(read_json "/etc/yunohost/apps/$YNH_APP_INSTANCE_NAME/manifest.json" 'version' 2> /dev/null || echo '20160501-7')
+    last_version=$(read_manifest 'version')
+    if [ "${version}" = "${last_version}" ] && [ "$YNH_APP_UPGRADE_TYPE" != "UPGRADE_FORCED" ]; then
+        ynh_print_info "Up-to-date, nothing to do"
+        ynh_die "" 0
+    fi
+}
+
+ynh_version_gt ()
+{
+    dpkg --compare-versions "$1" gt "$2"
+}
