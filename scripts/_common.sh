ed2bfc755421ac5970f519c2c40992b4400d7701
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-05-31 21:18:56 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="curl unzip libncurses5 postgresql postgresql-contrib imagemagick ffmpeg exiftool libimage-exiftool-perl"
+#REMOVEME? pkg_dependencies="curl unzip libncurses5 postgresql postgresql-contrib imagemagick ffmpeg exiftool libimage-exiftool-perl"
 
 #=================================================
 # PERSONAL HELPERS
